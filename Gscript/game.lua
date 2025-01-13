3eb1bde31e529da292788bedae2d66e2887cca24
Kyberkant <140261228+Kyberkant@users.noreply.github.com>
2025-01-13 19:46:05 +0100

    menu updated

--- Gscript/game.lua
+++ Gscript/game.lua
@@ -3,14 +3,20 @@ function createGame()
         state = {
             menu = false,
             story = false,
+            freeplay = false,
             setting = false,
+            credits = false,
+            achievements = false,
             void = true
         },
 
         switchGameState = function(self, state)
             self.state.menu = state == "menu"
             self.state.story = state == "story"
+            self.state.freeplay = state == "freeplay"
             self.state.setting = state == "setting"
+            self.state.credits = state == "credits"
+            self.state.achievements = state == "achievements"
             self.state.void = state == "void"
         end
     }
--- Gscript/menuButtons.lua
+++ Gscript/menuButtons.lua
@@ -3,18 +3,28 @@ function createButtons()
     local x = love.graphics.getWidth() / 4
     --y
     local y = y or 100
+    local sx = sx or 1
+    local sy = sy or 1
     --name
     local storyButton = love.graphics.newImage("assets/images/menu/story.png")
-    local freeplayButton = nil
-    local settingButton = nil
+    local freeplayButton = love.graphics.newImage("assets/images/menu/freeplay.png")
+    local settingsButton = love.graphics.newImage("assets/images/menu/settings.png")
     
     local creditsButton = nil
-    local achievementsButton = nil
+    local achievementsButton = love.graphics.newImage("assets/images/menu/achievements.png")
 
     return {
-        makeButton = function(self,name,x,y)
-            love.graphics.draw(name,x,y)
-        end
+        makeButton = function(self,image,x,y,sx,sy)
+            love.graphics.draw(image,x,y,0,sx,sy)
+        end,
+
+    names = {
+       storyButton = storyButton,
+            freeplayButton = freeplayButton,
+            settingsButton = settingsButton,
+            creditsButton = creditsButton,
+            achievementsButton = achievementsButton
+      }
     }
 end
 
